8bad894ac0ce268b560a3dc68aed235050ad275e
Ronaldo paroski <73145935+RonaldoParoski@users.noreply.github.com>
2023-03-16 16:46:06 -0300

    mochila-de-viagem

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-
 const form = document.querySelector('#novoItem')
 const lista = document.querySelector('.lista')
+const itens = [] //array para armazenar o itens e armazenar no local storage
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault() //interrompe que os dados fiquem na pagina 
-//    const nome = evento.target.elements['nome'].value
-//    const quantidade = evento.target.elements['quantidade'].value
 
-   criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = "" // todas as vezes q enviar um novo cadastro ele limpa os campos do form
 })
 
 
 //FUNÇÃO QUE RECEBE OS CAMPOS DO FORM E CRIA O ITEM
-function criaElemento(nome, quantidade){
+function criaElemento(nome, quantidade) {
 
-        // CRIA UM NOVO ITEM NA LISTA
+    // CRIA UM NOVO ITEM NA LISTA
     const novoItem = document.createElement('li')
     novoItem.classList.add("item") // PEGA A MESMA CLASSE DO LI DO HTML
 
@@ -26,5 +30,17 @@ function criaElemento(nome, quantidade){
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
+
+    // localStorage.setItem("nome", nome) //salva no local storage da pagina web o dado, como um banco
+    // localStorage.setItem("quantidade", quantidade)
+
+    const itemAtual = { //cria um objeto pra receber mais de um dado e armazenar no local storage
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual) //coloca os itens do array no json
+
+    localStorage.setItem("item", JSON.stringify(itens)) //JSON.stringfy-> transforma um elemento em uma string
 
 }
