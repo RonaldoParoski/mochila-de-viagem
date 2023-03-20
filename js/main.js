3334e135a1aee810712cf5e5679b4cc0188644a0
Ronaldo paroski <73145935+RonaldoParoski@users.noreply.github.com>
2023-03-20 11:57:04 -0300

    mochila-de-viagem

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('#novoItem')
 const lista = document.querySelector('.lista')
-const itens = [] //array para armazenar o itens e armazenar no local storage
+// const itens = [] //array para armazenar o itens e armazenar no local storage
+const itens = JSON.parse(localStorage.getItem("itens")) || [] //consulta no localstorage se tem itens ou nao
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault() //interrompe que os dados fiquem na pagina 
@@ -8,7 +13,17 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+
+    const itemAtual = { //cria um objeto pra receber mais de um dado e armazenar no local storage
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual) //coloca os itens do array no json
+
+    localStorage.setItem("itens", JSON.stringify(itens)) //JSON.stringfy-> transforma um elemento em uma string
 
     nome.value = ""
     quantidade.value = "" // todas as vezes q enviar um novo cadastro ele limpa os campos do form
@@ -16,7 +31,7 @@ form.addEventListener("submit", (evento) => {
 
 
 //FUNÇÃO QUE RECEBE OS CAMPOS DO FORM E CRIA O ITEM
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
     // CRIA UM NOVO ITEM NA LISTA
     const novoItem = document.createElement('li')
@@ -24,23 +39,14 @@ function criaElemento(nome, quantidade) {
 
     // pega o numero que esta na quantidade
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
 
     novoItem.appendChild(numeroItem) // appendChild coloca um elemento dentro do outro
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 
     // localStorage.setItem("nome", nome) //salva no local storage da pagina web o dado, como um banco
     // localStorage.setItem("quantidade", quantidade)
-
-    const itemAtual = { //cria um objeto pra receber mais de um dado e armazenar no local storage
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual) //coloca os itens do array no json
-
-    localStorage.setItem("item", JSON.stringify(itens)) //JSON.stringfy-> transforma um elemento em uma string
 
 }
